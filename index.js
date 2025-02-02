754e6a3a61feac77fde13feb4e3f9b8fb744c290
shubham <shubhamjha555@gmail.com>
2025-02-02 10:58:00 +0530

    base code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // lets start implementing expresjs
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
